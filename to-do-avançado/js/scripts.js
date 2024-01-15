d4cfbb9310a7d5eba071b150379bafc1aa3bb63f
lucasalc25 <lucas.alc25@gmail.com>
2024-01-15 16:53:42 -0400

    Continuação Projeto To Do
    Edição de tarefas

--- to-do-avançado/js/scripts.js
+++ to-do-avançado/js/scripts.js
@@ -5,6 +5,13 @@ const taskList = document.querySelector("#task-list");
 const editForm = document.querySelector("#edit-form");
 const cancelEditBtn = document.querySelector("#cancel-edit-btn");
 const removeBtn = document.querySelector(".remove-task");
+const editInput = document.querySelector("#edit-input");
+const editBtn = document.querySelector("#edit-btn");
+const searchInput = document.querySelector("#search-input");
+const eraseBtn = document.querySelector("#remove-btn");
+const filterBtn = document.querySelector("#filter-select");
+
+let oldInputValue;
 
 // Funções
 const saveTask = (TaskTitle) => {
@@ -40,6 +47,24 @@ const saveTask = (TaskTitle) => {
      taskInput.focus();
 }
 
+const toggleForms = () => {
+    taskForm.classList.toggle("hide");
+    editForm.classList.toggle("hide");
+    taskList.classList.toggle("hide");
+}
+
+const updateTask = (TaskTitle) => {
+    const tasks = document.querySelectorAll(".task");
+
+    tasks.forEach((task) => {
+        let taskTitle = task.querySelector("h3");
+
+        if(taskTitle.innerText === oldInputValue) {
+            taskTitle.innerText = TaskTitle;
+        }
+    })
+}
+
 // Eventos
 taskForm.addEventListener("submit", (e) => {
     e.preventDefault();
@@ -55,12 +80,38 @@ taskForm.addEventListener("submit", (e) => {
 document.addEventListener("click", (e) => {
     const targetElement = e.target;
     const parentElement = targetElement.closest("div");
+    let taskTitle;
 
-    if(targetElement.classList.contains("finish-task")) {
-        parentElement.classList.toggle("done");
+    if(parentElement && parentElement.querySelector("h3")) {
+        taskTitle = parentElement.querySelector("h3").innerText;
     }
 
-    if(targetElement.classList.contains("remove-task")) {
-        parentElement.remove();
+    if(targetElement.classList.contains("finish-task")) { parentElement.classList.toggle("done"); }
+
+    if(targetElement.classList.contains("remove-task")) { parentElement.remove(); }
+
+    if(targetElement.classList.contains("edit-task")) { 
+        toggleForms();
+        editInput.value = taskTitle;
+        oldInputValue = taskTitle;
     }
+})
+
+cancelEditBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    toggleForms();
+})
+
+editBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    const newEditInput = editInput.value;
+
+    if(newEditInput) {
+        // Atualizar
+        updateTask(newEditInput);
+    }
+
+    toggleForms();
 })
