1601d8505931f35b535f844a5513e3de9ba48390
lucasalc25 <lucas.alc25@gmail.com>
2024-01-16 00:00:54 -0400

    Continuação Projeto To Do
    Pesquisa e Filtro

--- to-do-avançado/js/scripts.js
+++ to-do-avançado/js/scripts.js
@@ -8,40 +8,47 @@ const removeBtn = document.querySelector(".remove-task");
 const editInput = document.querySelector("#edit-input");
 const editBtn = document.querySelector("#edit-btn");
 const searchInput = document.querySelector("#search-input");
-const eraseBtn = document.querySelector("#remove-btn");
+const eraseBtn = document.querySelector("#erase-btn");
 const filterBtn = document.querySelector("#filter-select");
 
 let oldInputValue;
 
 // Funções
-const saveTask = (TaskTitle) => {
+const saveTask = (taskTitle, done = 0, save = 1) => {
      const task = document.createElement('div');
      task.classList.add("task");
      
      const title = document.createElement('h3');
-     title.innerText = TaskTitle;
-
+     title.innerText = taskTitle;
      task.appendChild(title);
 
      const finishBtn = document.createElement('button');
      finishBtn.classList.add("finish-task");
      finishBtn.innerHTML = '<i class="fa-solid fa-check"></i>'
-
      task.appendChild(finishBtn);
 
      const editBtn = document.createElement('button');
      editBtn.classList.add("edit-task");
      editBtn.innerHTML = '<i class="fa-solid fa-pen"></i>'
-
      task.appendChild(editBtn);
 
      const removeBtn = document.createElement('button');
      removeBtn.classList.add("remove-task");
      removeBtn.innerHTML = '<i class="fa-solid fa-xmark"></i>'
-
      task.appendChild(removeBtn);
 
+    // Utilizando dados da LocalStorage
+    if(done) {
+        task.classList.add("done")
+    }
+
+    if(save) {
+        saveTaskLocalStorage({ taskTitle, done });
+    }
+
      taskList.appendChild(task);
+
+     saveTaskLocalStorage (task);
 
      taskInput.value = "";
      taskInput.focus();
@@ -53,19 +60,56 @@ const toggleForms = () => {
     taskList.classList.toggle("hide");
 }
 
-const updateTask = (TaskTitle) => {
+const updateTask = (taskTitle) => {
     const tasks = document.querySelectorAll(".task");
 
     tasks.forEach((task) => {
-        let taskTitle = task.querySelector("h3");
+        let newtaskTitle = task.querySelector("h3");
 
-        if(taskTitle.innerText === oldInputValue) {
-            taskTitle.innerText = TaskTitle;
+        if(newtaskTitle.innerText === oldInputValue) {
+            newtaskTitle.innerText = taskTitle;
         }
     })
 }
 
+const getSearchTasks = (search) => {
+    const tasks = document.querySelectorAll(".task");
+
+    tasks.forEach((task) => {
+        let taskTitle = task.querySelector("h3").innerText.toLowerCase();
+
+        const normalizeSearch = search.toLowerCase();
+
+        task.style.display = "flex";
+
+        if(!taskTitle.includes(search)) {
+            task.style.display = "none";
+        }
+
+    })
+};
+
+const filterTaks = (filterValue) => {
+    const tasks = document.querySelectorAll(".task");
+
+    switch(filterValue) {
+        case "all":
+            tasks.forEach((task) => task.style.display = "flex");
+            break;
+        case "done":
+            tasks.forEach((task) => task.classList.contains("done") ? task.style.display = "flex" : task.style.display = "none");
+            break;
+        case "pending":
+            tasks.forEach((task) => !task.classList.contains("done") ? task.style.display = "flex" : task.style.display = "none");
+            break; 
+    
+        default:
+            break;        
+    }
+}
+
 // Eventos
+// Tarefas
 taskForm.addEventListener("submit", (e) => {
     e.preventDefault();
 
@@ -77,6 +121,7 @@ taskForm.addEventListener("submit", (e) => {
     }
 })
 
+// Cliques em elementos da página
 document.addEventListener("click", (e) => {
     const targetElement = e.target;
     const parentElement = targetElement.closest("div");
@@ -97,12 +142,14 @@ document.addEventListener("click", (e) => {
     }
 })
 
+// Cancelar edição
 cancelEditBtn.addEventListener("click", (e) => {
     e.preventDefault();
 
     toggleForms();
 })
 
+// Confirmar edição
 editBtn.addEventListener("click", (e) => {
     e.preventDefault();
 
@@ -115,3 +162,45 @@ editBtn.addEventListener("click", (e) => {
 
     toggleForms();
 })
+
+// Pesquisa
+searchInput.addEventListener("keyup", (e) => {
+
+    const search = e.target.value;
+
+    getSearchTasks(search);
+})
+
+// Limpar pesquisa
+eraseBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    searchInput.value = "";
+
+    searchInput.dispatchEvent(new Event("keyup"));
+})
+
+// Filtro
+filterBtn.addEventListener("change", (e)=> {
+    const filterValue = e.target.value;
+
+    filterTaks(filterValue);
+})
+
+// Local Storage
+const getTasksLocalStorage = () => {
+    const tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+    return tasks;
+};
+
+const saveTaskLocalStorage = (task) => {
+
+    // Pegar todas as tarefas
+    const tasks = getTasksLocalStorage();
+
+    // Adicionar nova tarefa no array
+    tasks.push(task);
+
+    //Salvar tudo na LocalStorage
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+};
